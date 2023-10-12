819811496712a728a70927617ded6ebcdac52282
Anouar El maaroufi <ael-maar@student.1337.ma>
2023-10-12 17:08:24 +0100

    Initialize the name attribute in the copy constructor and default constructor

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,13 +6,13 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 10:43:35 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/10/11 13:30:49 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:06:06 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form(): is_signed(false), grade_sign(10), grade_exec(10) {}
+Form::Form(): name("Unknown"), is_signed(false), grade_sign(10), grade_exec(10) {}
 
 Form::Form(std::string const &name, int const grade_sign, int const grade_exec): name(name), is_signed(false), grade_sign(grade_sign), grade_exec(grade_exec) {
     if (grade_sign < 1 || grade_exec < 1)
@@ -21,7 +21,7 @@ Form::Form(std::string const &name, int const grade_sign, int const grade_exec):
         throw Form::GradeTooLowException();
 }
 
-Form::Form(Form const &old_obj): is_signed(old_obj.is_signed), grade_sign(old_obj.grade_sign), grade_exec(old_obj.grade_exec) {}
+Form::Form(Form const &old_obj): name(old_obj.name), is_signed(old_obj.is_signed), grade_sign(old_obj.grade_sign), grade_exec(old_obj.grade_exec) {}
 
 Form &Form::operator=(Form const &other) {
     if (this != &other)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 11:47:49 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/10/11 10:15:05 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:29:01 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,5 @@
 
 int main(void)
 {
-    // Grade during initialization is high
-    std::cout << "-----Test1-----" << std::endl;
-    try
-    {
-        Bureaucrat b1("Anouar", -1);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    // Grade during initialization is low
-    std::cout << "-----Test2-----" << std::endl;
-    try
-    {
-        Bureaucrat b2("Malak", 151);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    // Grade is out of range
-    std::cout << "-----Test3-----" << std::endl;
-    try
-    {
-        Bureaucrat b1("Anouar", 1);
-        std::cout << b1 << std::endl;
-        b1.IncrementGrade();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    std::cout << "-----Test4-----" << std::endl;
-    try
-    {
-        Bureaucrat b1("Anouar", 150);
-        std::cout << b1 << std::endl;
-        b1.DecrementGrade();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    
 }
